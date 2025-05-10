ce786741a97bd377a998aec59c475e9f748d83bc
Armin <dante6363@protonmail.com>
2025-05-10 15:38:27 +0330

    - implement chainlink VRF - wirte enum for raffle states - write event for picking a winner - introduction of CEI

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -23,32 +23,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title A sample Raffle smart contract
  * @author Armin Siahmansour
  * @notice This contract is for creating a sample raffle
  * @dev Implement chainlink VRFv2.5
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /*errors*/
     error Raffle__SendMoreToEnterRaffle();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
 
+    /*type declarations*/
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
     /*state variables*/
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
-    uint256 private immutable sag;
+    uint256 private immutable i_interval;
+    uint256 private immutable i_subscriptionId;
+    bytes32 private immutable i_keyHash;
+    uint32 private immutable i_callbackGasLimit;
+    address private immutable i_vrfCoordinator;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+    address payable private s_recentWinner;
+    RaffleState private s_raffleState;
+
     /*events*/
     event RaffleEnterd(address indexed player);
+    event PickWinner(address indexed winner);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(i_vrfCoordinator) {
+        i_vrfCoordinator = vrfCoordinator;
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+
+        s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
     }
 
     function enterRaffle() public payable {
         // require(msg.value>=i_entranceFee,"send more money to enter the raffle"); //Not optimize cause we need do store a string
         // require(msg.value>=i_entranceFee,Raffle__SendMoreToEnterRaffle()); //1-only after solidity 0.18.26
         //2-need a complex compiler to run and take more time to compile 3= more gas and not optimized.
-
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
@@ -56,7 +94,53 @@ contract Raffle {
         emit RaffleEnterd(msg.sender);
     }
 
-    function pickWinner() public {}
+    //1-Get a random number
+    //2-Use that number to get the winner
+    //3.Be automatically called
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
+            revert();
+        }
+        s_raffleState = RaffleState.CALCULATING;
+
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+    }
+
+    //CEI Checks, Effects, Interactions patterm
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {
+        //Checks
+        //no checks here
+
+        //Effects(Internal Contract State)
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit PickWinner(winner);
+
+        //Interactions(External Contract Interactions)
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
 
     /*getter functions*/
     function getEntranceFee() external view returns (uint256) {
